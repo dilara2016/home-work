8ef440ebfb333da8c4c7bc6018a0ae9aae5a72e8
dilara2016 <m.123.hunain@gmail.com>
2024-10-05 12:36:34 +0200

    homework.py

---
+++ homework.py
@@ -0,0 +1,18 @@
+name="dilara"
+age = 8
+is_student = False
+weight = 20.0
+#printing diffrent varibles and there data type
+print("Name :",name)
+print("data type of Name is",type(dilara))
+print("Age:",age)
+print("data type of Age is",type(8))
+print("Is_student :",is_student)
+print("data type of Is_student is",type(False))
+print("Weigt:",weight)
+print("data type of Weight is",type(20))
+#type casting to convert the datatype of varibles
+print("/n  After type casting....")
+age = str(age)
+print("Age:",age)
+print("data type of Age is",type(8))
